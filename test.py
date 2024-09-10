9122c8b114f809c74b07afd1b91d4ce81d726407
tathianamb <tm_barchi@hotmail.com>
2024-09-10 17:38:00 -0300

    test script

---
+++ test.py
@@ -0,0 +1,18 @@
+from MyVecStore.vector_store import DataToVectorStoreProcessor
+
+
+json_config = {
+    'file_path': 'test_file.json',
+    'jq_schema': '.[] | {name: .name, post_title: .post_title, post_content: .post_content, taxonomy_name_values: .taxonomy_name_values, post_date: .post_date}'
+}
+
+processor = DataToVectorStoreProcessor(
+    source_type="json",
+    source_config=json_config,
+    chunk_size=500,
+    chunk_overlap=100
+)
+
+processor.process()
+
+print("Artigos processados e armazenados com sucesso na vector store!")
