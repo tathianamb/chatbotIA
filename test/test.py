5a20f329984e51e0c7f99628cfadf6d47da33e70
Tathiana <tm_barchi@hotmail.com>
2024-09-11 18:19:16 -0300

    test adjust

--- test/test.py
+++ test/test.py
@@ -1,8 +1,12 @@
 from vendor.vector_store import DataToVectorStoreProcessor
+import os
 
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+test_file_path = os.path.normpath(os.path.join(script_dir, 'test_file.json'))
 
 json_config = {
-    'file_path': 'test_file.json',
+    'file_path': test_file_path,
     'jq_schema': '.[] | {name: .name, post_title: .post_title, post_content: .post_content, taxonomy_name_values: .taxonomy_name_values, post_date: .post_date}'
 }
 
