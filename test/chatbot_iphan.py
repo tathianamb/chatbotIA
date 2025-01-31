3ad31b52417b1b1813f2683e29a7b75306320ae3
Tathiana <tm_barchi@hotmail.com>
2025-01-31 09:58:56 -0300

    fix api url nad add loop

--- test/chatbot_iphan.py
+++ test/chatbot_iphan.py
@@ -14,39 +14,25 @@ logging.basicConfig(
 logging.getLogger('faiss').setLevel(logging.WARNING)
 
 
-def test_questions_distance_strategies():
-    vector_file_name = 'sql_l2'
-
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    index_path = os.path.join(script_dir, '..', 'data', vector_file_name)
-
-    vector_store_paths = {
-        vector_file_name: os.path.normpath(index_path)
-    }
-    questions = [
-        "Quais foram os principais objetivos das reuniões on-line com os forrozeiros do estado do Piauí para a construção do Plano de Salvaguarda das Matrizes Tradicionais do Forró?",
-        "Como se deu o processo de mobilização e discussão para a elaboração do Plano Nacional de Salvaguarda da Literatura de Cordel na Bahia?",
-        "Quais diretrizes foram discutidas nas reuniões com o Coletivo Carmo para a promoção e salvaguarda dos Mestres de Capoeira Angola?",
-        "Quais foram os principais pontos abordados na roda de conversa sobre previdência voltada para os capoeiristas?",
-        "Quais ações foram realizadas para a salvaguarda da Tava Guarani em colaboração com o Centro de Referência Indígena-Afro no Rio Grande do Sul?"
-    ]
-    api_url = "http://www.api.nice.ibict.br/ibictLLM"
-
-    for strategy, vector_store_path in vector_store_paths.items():
-        logging.info('Testing with distance strategy: %s', strategy)
-        chatbot = IBICTChatbot(api_url=api_url, api_key=api_key)
-        for question in questions:
-            chatbot.get_response(user_message=question, vector_store_path=vector_store_path)
-            time.sleep(5)
-
 if __name__ == "__main__":
     api_key = os.getenv('LLMIBICT')
     if not api_key:
         raise ValueError("API Key não definida. Por favor, configure a variável de ambiente 'LLMIBICT'.")
 
-    test_questions_distance_strategies()
+    api_url = "https://api-nice.ibict.br/ibictLLM"
 
-    #api_url = "http://www.api.nice.ibict.br/ibictLLM"
-    #vector_store_path = "popularizacao_da_ciencia.csv_index"
-    #chatbot = IBICTChatbot(api_url, api_key)
-    #chatbot.run(vector_store_path)
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    index_path = os.path.join(script_dir, '..', 'data', 'sql_l2')
+    vector_store_path = os.path.normpath(index_path)
+
+    chatbot = IBICTChatbot(api_url, api_key)
+
+    while True:
+        pergunta = input("Pergunta (ou digite 'sair' para encerrar): ")
+
+        if pergunta.lower() == "sair":
+            print("Encerrando o chatbot. Até mais!")
+            break
+
+        resposta = chatbot.get_response(pergunta, vector_store_path)
+        print(f"Resposta: {resposta}")
