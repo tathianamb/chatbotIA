44d4eee5781a1139347a967baa7e9bf08a8f76bf
Tathiana <tm_barchi@hotmail.com>
2025-03-20 11:59:33 -0300

    fix base model url, add current time info, fix get and post functions

--- src/chatbot_api.py
+++ src/chatbot_api.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from vendor.rag_model import chatbot
+from vendor.rag_model import Chatbot
 import logging
 import os
+from datetime import datetime
+import pytz
 
 
 class ChatRequest(BaseModel):
@@ -19,23 +21,39 @@ logging.basicConfig(
 logging.getLogger('faiss').setLevel(logging.WARNING)
 
 app = FastAPI()
-api_url = "https://api-nice.ibict.br/ibictLLM"
-api_key = os.getenv('LLMIBICT')
-
+api_ollama = "http://localhost:11434/api/chat"
 vector_file_name = 'sql_l2'
 script_dir = os.path.dirname(os.path.realpath(__file__))
 vector_store_path = os.path.join(script_dir, '..', 'data', vector_file_name)
 
-chatbot = IBICTChatbot(api_url=api_url, api_key=api_key)
+chatbot = Chatbot(api_ollama=api_ollama, model_llm="llama3.1:8b", model_embeddings="nomic-embed-text", temperature=0.1, seed=100)
+
+def get_current_time():
+    return datetime.now(pytz.utc).isoformat()
 
 
-@app.post("/chatbot-iphan-obs/")
+@app.get("/")
+def read_root():
+    return {"message": "Bem-vindo à API do Chatbot! Use o endpoint /chatbot para fazer uma pergunta."}
+
+
+@app.post("/chatbot/")
 async def ask_chatbot(chat_request: ChatRequest):
-    user_message = chat_request.question
+    try:
+        answer = chatbot.get_response_RAGChatbot(ChatRequest.question, vector_store_path, k=2)
 
-    response = chatbot.get_response(user_message=user_message, vector_store_path=vector_store_path)
-    return {"response": response}
+        response_body = {
+            "model": "llama3.1:8b",
+            "created_at": get_current_time(),
+            "message": {
+                "role": "assistant",
+                "content": answer
+            }
+        }
 
+        return response_body
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     import uvicorn
