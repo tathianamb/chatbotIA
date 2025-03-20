f4ff56831a958200dc57cde3d2b60127e431e033
Tathiana <tm_barchi@hotmail.com>
2025-03-20 11:58:08 -0300

    creating new class

---
+++ vendor/base_agent.py
@@ -0,0 +1,35 @@
+import json
+import requests
+import logging
+
+
+class BaseAgent:
+
+    def __init__(self, api_ollama, model_llm, temperature, seed):
+        self.api_ollama = api_ollama
+        self.payload = {
+            "model": model_llm,
+            "stream": False,
+            "options": {
+                "seed": seed,
+                "temperature": temperature
+            }
+        }
+        logging.info(f'Initial configuration: {model_llm}.')
+
+    def _set_messages(self, role, content):
+        pass
+
+    def _send_request(self):
+        json_payload = json.dumps(self.payload, ensure_ascii=False, indent=2)
+        logging.info('Sending request to API')
+        logging.debug('Sent payload: %s', json_payload)
+        try:
+            response = requests.post(self.api_ollama, json=self.payload)
+            response.raise_for_status()
+            logging.debug('Received response from API with status code: %d', response.status_code)
+            logging.debug('Response content: %s', response.text[:500])
+            return response.json()
+        except requests.RequestException as e:
+            logging.error(f'Request failed: {e}')
+            raise
