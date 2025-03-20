b9baa68d42c90b347ea5c0427328acaff9085821
Tathiana <tm_barchi@hotmail.com>
2025-03-20 12:02:07 -0300

    update class (considering base agent), fix name params

--- vendor/rag_model.py
+++ vendor/rag_model.py
@@ -1,39 +1,31 @@
 import json
-import requests
 from langchain_ollama import OllamaEmbeddings
 import logging
 import faiss
 import numpy as np
 import os
 import time
+from vendor.base_agent import BaseAgent
+from vendor.query_rewrite import AgentQR
 
-class chatbot:
 
-    def __init__(self, api_url, api_key, model='llama3.1:8b', model_name="nomic-embed-text", temperature=None, num_ctx=None, keep_alive=None, seed=None):
-        self.api_url = api_url
-        self.api_key = api_key
-        self.model_name = model_name
-        self.headers = {'Content-Type': 'application/json', 'apikey': api_key}
-        self.payload = {
-            'model': model,
-            'stream': False,
-            'keep_alive': keep_alive or '60m',
-            'options': {
-                'seed': seed or 105,
-                'temperature': temperature or 0.7,
-                'num_ctx': num_ctx or 8192,
-            },
-            'messages': []
-        }
-        logging.info(f'Initial payload configuration: {json.dumps(self.payload, ensure_ascii=False, indent=2)}')
+class Chatbot(BaseAgent):
+
+    def __init__(self, api_ollama, model_llm, model_embeddings, temperature, seed):
+        super().__init__(api_ollama, model_llm, temperature, seed)
+        self.payload["messages"] = []
+        self.model_embeddings = model_embeddings
+        logging.info(f'Initial configuration: {model_llm}, {model_embeddings}.')
 
     def _set_messages(self, role, content):
-        message = {'role': role, 'content': content}
-        self.payload['messages'].append(message)
+        super()._set_messages(role, content)
+        message = {"role": role, "content": content}
+        self.payload["messages"].append(message)
         logging.info(f"Added message to payload - {message}")
 
+        logging.info(f"{message}")
 
-    def load_vector_store_and_chunks(self, index_path):
+    def _load_vector_store_and_chunks(self, index_path):
         index = faiss.read_index(f"{index_path}_index")
 
         with open(f"{index_path}_docs.json", 'r', encoding='utf-8') as f:
@@ -41,16 +33,16 @@ class chatbot:
 
         return index, text_chunks
 
-    def _retrieve_docs(self, user_question, vector_store_path, k=1):
-
+    def _retrieve_docs(self, context, vector_store_path, k=1):
+        print(f"Context to retrive docs: {context}\n")
         if os.getenv("OLLAMA_BASE_URL"):
-            embeddings = OllamaEmbeddings(base_url=os.getenv("OLLAMA_BASE_URL"), model=self.model_name)
+            embeddings = OllamaEmbeddings(base_url=os.getenv("OLLAMA_BASE_URL"), model=self.model_embeddings)
         else:
             embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
-        user_question_embedding = np.array(embeddings.embed_query(user_question)).astype("float32").reshape(1, -1)
+        user_question_embedding = np.array(embeddings.embed_query(context)).astype("float32").reshape(1, -1)
 
-        index, text_chunks = self.load_vector_store_and_chunks(vector_store_path)
+        index, text_chunks = self._load_vector_store_and_chunks(vector_store_path)
 
         start_time = time.perf_counter_ns()
         D, I = index.search(user_question_embedding, k)
@@ -61,7 +53,7 @@ class chatbot:
         results = [text_chunks[i] for i in I[0]]
 
         retrieved_docs_info = [
-            {"document": results[i], "distance":  float(D[0][i])}
+            {"document": results[i], "distance": float(D[0][i])}
             for i in range(len(results))
         ]
 
@@ -71,37 +63,24 @@ class chatbot:
         return results
 
     def _build_context(self, docs):
-        return "\n".join([doc for doc in docs])
+        return " ".join([doc for doc in docs])
 
-    def _send_request(self):
-        logging.info('Sending request to API')
-        logging.debug('Sent payload: %s',
-                     json.dumps(self.payload, ensure_ascii=False, indent=2))
-        try:
-            response = requests.post(self.api_url, json=self.payload, headers=self.headers)
-            response.raise_for_status()
-            logging.debug('Received response from API with status code: %d', response.status_code)
-            logging.debug('Response content: %s', response.text[:500])
-            return response.json()
-        except requests.RequestException as e:
-            logging.error(f'Request failed: {e}')
-            raise
+    def get_response_RAGChatbot(self, user_message, vector_store_path, k):
 
-    def get_response(self, user_message, vector_store_path, k, to_rewrite_query=False):
-        self._set_messages("user", user_message)
-        terms_retrieve_docs = user_message
+        if len(self.payload["messages"]) > 0:
+            qr_agent = AgentQR(api_ollama="http://localhost:11434/api/generate", model_llm="deepseek-llm:7b", temperature=0.1, seed=100)
+            user_message = qr_agent.get_response_AgentQR(last_msg=user_message, historical_payload=self.payload)
 
-        docs = self._retrieve_docs(terms_retrieve_docs, vector_store_path, k)
+        docs = self._retrieve_docs(user_message, vector_store_path, k)
         context = self._build_context(docs)
 
-        self._set_messages("system", f"Com base nas informações disponíveis, utilize uma linguagem simpática para fornecer uma resposta informativa. Aqui está o contexto: {context}")
+        self._set_messages("system",
+                           f"Utilize uma linguagem simpática para fornecer uma resposta informativa o contexto. Se não souber a resposta, peça para tentar com outra pergunta. Contexto: {context}")
+        self._set_messages("user", user_message)
 
         response_data = self._send_request()
         answer = response_data.get("message", {}).get("content", "No content found.")
 
         self._set_messages("chatbot", answer)
-
-        self.payload['messages'].clear()
-        logging.info('==================================== end question ====================================')
 
         return answer
