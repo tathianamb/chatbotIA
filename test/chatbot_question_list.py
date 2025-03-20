e27b9df13a8c811249de5a2b3c14209cf58a1345
Tathiana <tm_barchi@hotmail.com>
2025-03-20 12:00:21 -0300

    fix package call name, update ollama api,

--- test/chatbot_question_list.py
+++ test/chatbot_question_list.py
@@ -1,4 +1,4 @@
-from vendor.rag_model import chatbot
+from vendor.rag_model import Chatbot
 import os
 import logging
 import time
@@ -18,7 +18,7 @@ def test_questions_distance_strategies():
     vector_file_name = 'sql_l2' #sql_inner_product sql_l2
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
-    index_path = os.path.join(script_dir, '..', 'data', 'raw_level', vector_file_name)
+    index_path = os.path.join(script_dir, '..', 'data', vector_file_name)
 
     vector_store_paths = {
         vector_file_name: os.path.normpath(index_path)
@@ -30,18 +30,15 @@ def test_questions_distance_strategies():
         "Quais foram os principais pontos abordados na roda de conversa sobre previdência voltada para os capoeiristas?",
         "Quais ações foram realizadas para a salvaguarda da Tava Guarani em colaboração com o Centro de Referência Indígena-Afro no Rio Grande do Sul?"
     ]
-    api_url = "https://api-nice.ibict.br/ibictLLM"
+    api_url = "http://localhost:11434/api/chat"
 
     for strategy, vector_store_path in vector_store_paths.items():
         logging.info('Testing with distance strategy: %s', strategy)
-        chatbot = chatbot(api_url=api_url, api_key=api_key)
+        chatbot = Chatbot(api_url=api_url)
         for question in questions:
-            chatbot.get_response(user_message=question, vector_store_path=vector_store_path, k=1)
+            response = chatbot.get_response(user_message=question, vector_store_path=vector_store_path, k=1)
+            print(f"response: {response}")
             time.sleep(5)
 
 if __name__ == "__main__":
-    api_key = os.getenv('LLMIBICT')
-    if not api_key:
-        raise ValueError("API Key não definida. Por favor, configure a variável de ambiente 'LLMIBICT'.")
-
     test_questions_distance_strategies()
