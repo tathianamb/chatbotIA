7a50e34cc8f437493a56a1fc673088a01ab1ac28
Tathiana <tm_barchi@hotmail.com>
2025-03-21 16:57:47 -0300

    fix address, add model_qr, add verifications,

--- src/chatbot_api.py
+++ src/chatbot_api.py
@@ -1,15 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from vendor.rag_model import Chatbot
 import logging
 import os
 from datetime import datetime
 import pytz
-
-
-class ChatRequest(BaseModel):
-    question: str
-
 
 logging.basicConfig(
     filename='../log/rag_model_run.log',
@@ -20,13 +15,21 @@ logging.basicConfig(
 
 logging.getLogger('faiss').setLevel(logging.WARNING)
 
+class ChatRequest(BaseModel):
+    question: str
+
 app = FastAPI()
-api_ollama = "http://localhost:11434/api/chat"
+api_ollama = "http://ollama:11434/api/chat"
 vector_file_name = 'sql_l2'
 script_dir = os.path.dirname(os.path.realpath(__file__))
 vector_store_path = os.path.join(script_dir, '..', 'data', vector_file_name)
 
-chatbot = Chatbot(api_ollama=api_ollama, model_llm="llama3.1:8b", model_embeddings="nomic-embed-text", temperature=0.1, seed=100)
+model_llm = "llama3.1:8b"
+model_embeddings = "nomic-embed-text"
+model_qr = "deepseek-llm:7b"
+
+chatbot = Chatbot(api_ollama=api_ollama, model_llm=model_llm, model_embeddings=model_embeddings, model_qr=model_qr,
+                  temperature=0.1, seed=100)
 
 def get_current_time():
     return datetime.now(pytz.utc).isoformat()
@@ -37,13 +40,20 @@ def read_root():
     return {"message": "Bem-vindo à API do Chatbot! Use o endpoint /chatbot para fazer uma pergunta."}
 
 
-@app.post("/chatbot/")
+@app.post("/chatbot")
 async def ask_chatbot(chat_request: ChatRequest):
     try:
-        answer = chatbot.get_response_RAGChatbot(ChatRequest.question, vector_store_path, k=2)
+        if not chat_request.question.strip():
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="O campo 'question' não pode estar vazio."
+            )
+
+        answer = chatbot.get_response_RAGChatbot(chat_request.question, vector_store_path, k=2)
 
         response_body = {
-            "model": "llama3.1:8b",
+            "model_llm": model_llm,
+            "model_qr": model_qr,
             "created_at": get_current_time(),
             "message": {
                 "role": "assistant",
@@ -52,8 +62,14 @@ async def ask_chatbot(chat_request: ChatRequest):
         }
 
         return response_body
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        error_msg = f"Erro interno no servidor: {str(e)}"
+        logging.error(error_msg)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=error_msg
+        )
 
 if __name__ == "__main__":
     import uvicorn
